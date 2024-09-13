9ec54093e3707b1f9b1b0e5546c99787e225bd11
j4vito <jiruiz5@uc.cl>
2024-09-13 16:55:48 -0300

    feat(Api): deploy model into api

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,7 +1,7 @@
 import unittest
-
+import numpy as np
 from fastapi.testclient import TestClient
-from challenge import app
+from challenge.api import app
 
 
 class TestBatchPipeline(unittest.TestCase):
@@ -18,10 +18,11 @@ class TestBatchPipeline(unittest.TestCase):
                 }
             ]
         }
-        # when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0])) # change this line to the model of chosing
+        #when("xgboost.XGBClassifier").predict(ANY).thenReturn(np.array([0])) # change this line to the model of chosing
         response = self.client.post("/predict", json=data)
-        self.assertEqual(response.status_code, 200)
+        #response = self.client.get("/")#, json=data)
         self.assertEqual(response.json(), {"predict": [0]})
+        self.assertEqual(response.status_code, 200)
     
 
     def test_should_failed_unkown_column_1(self):
